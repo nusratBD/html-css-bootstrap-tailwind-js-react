309863fc4c33f7e4395cbb92793b850ed8e2999f
nusratBD <nusrat.hurain15@gmail.com>
2022-08-04 19:52:41 +0600

    Some more uses of caret(string literals)

--- Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module 31: Basic-ES6/create-string/dynamic-string.js
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module 31: Basic-ES6/create-string/dynamic-string.js
@@ -4,8 +4,24 @@ String Interpolation: To set the value of a variable with the name of a variable
 //quote('') System:
 let count = 5;
    let friend1 = '<h1 class="friend">Friend-'+ count +'</h1>'
-console.log(friend1);
+// console.log(friend1);
 
 //caret(``) System:
 let friend2 = `<h1 class="friend">Friend-${count}</h1>`;
-console.log(friend2);
+// console.log(friend2);
+
+//More use of caret: Caret is called template literals
+//01
+let fName = 'Nusrat';
+let lName = 'Hurain';
+let fullName = `${fName} ${lName}`;
+// console.log(fullName);
+//02
+let names = ['Abul', 'Babul', 'Kabul', 'Cabul', 'Jabul'];
+let nameLength = `<h1 class="friend">Friend Quantity-${names.length}</h1>`;//Except variable, we can use any valid JS expression within the string literals
+// console.log(nameLength);
+//03
+let city = 'Dhaka';
+let mySelf = `My names is ${fullName}. I live in ${city}. My friend quantity is ${names.length}.`;
+console.log(mySelf);
+
