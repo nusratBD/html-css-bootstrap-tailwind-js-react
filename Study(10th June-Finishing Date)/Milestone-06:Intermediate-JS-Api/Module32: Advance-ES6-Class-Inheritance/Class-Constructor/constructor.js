ca6dc04477d480656c67f37a9818121d6458d2a1
nusratBD <nusrat.hurain15@gmail.com>
2022-08-07 07:55:58 +0600

    access property inside of a constructor of a class

--- Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32: Advance-ES6-Class-Inheritance/Class-Constructor/constructor.js
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32: Advance-ES6-Class-Inheritance/Class-Constructor/constructor.js
@@ -8,14 +8,15 @@ class Myself{
     constructor(name, age, profession){
         this.name = name;
         this.age = age;
-        // this.profession = profession //If we access any properties inside the constructor, but we don't pass the parameter while calling the class then it will be undefined although we set the value intially
+        this.profession = profession //If we access any properties inside the constructor, we must provide the parameter while calling the class. To understand it, run result.
     }
     instruction(){
         console.log(this.name, 'Start a support session');//if we want to access any property inside the function we must use thid keyword(this.properties)
     }
 }
-let result = new Myself('Nusrat Hurain Nuha', 25);
-let result2 = new Myself('Sultana Kamal', 26);
-console.log(result, result2);
+let result = new Myself( 25);// output: Myself { name: 25, age: undefined, profession: undefined }
+let result2 = new Myself('Sultana Kamal', 26);// output: Myself { name: 'Sultana Kamal', age: 26, profession: undefined }
+let result3 = new Myself('Nusrat Hurain Nuha', 25, 'Software Engineer');
+console.log(result, result2, result3);
 result.instruction();//access the function from the class
 result2.instruction();
