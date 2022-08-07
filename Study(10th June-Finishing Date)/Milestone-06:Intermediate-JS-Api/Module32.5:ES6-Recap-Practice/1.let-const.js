7d0d7c29fffc3d4974d6269dfada1d320362b941
nusratBD <nusrat.hurain15@gmail.com>
2022-08-07 15:55:45 +0600

    Practiced let and const variable

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/1.let-const.js
@@ -0,0 +1,10 @@
+//let: when we can reassign to a variable, we use let
+let age = 25;
+console.log(age);
+age = 26;
+console.log(age);
+
+//const: When we can not reassign to a variable, we use const. If we try to reassign to a const variable, it will provide error
+const name = 'Nusrat Hurain';
+console.log(name);
+name ='Nuha';// not possible
