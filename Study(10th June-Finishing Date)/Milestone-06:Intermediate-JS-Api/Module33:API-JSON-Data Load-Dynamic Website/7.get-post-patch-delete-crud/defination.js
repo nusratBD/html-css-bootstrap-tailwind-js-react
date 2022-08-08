daf9226671c42bf4e5379e7cdb2f7d9e34d9806d
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 16:46:38 +0600

    Defination of GET, POST, PATCH, DELETE, PUT

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module33:API-JSON-Data Load-Dynamic Website/7.get-post-patch-delete-crud/defination.js
@@ -0,0 +1,7 @@
+/*
+GET: loading data from any site is 'GET'
+POST: Keeping data to server is 'POST'
+PATCH: Editing data to server is 'PATCH'
+DELETE: Delete data from sever is 'DELETE'
+PUT: near to post. If the data does not exist, it will add otherwise replace.
+*/
