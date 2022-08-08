1ddb6361c65f62a57941cd3dce3b251424361882
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 10:21:07 +0600

    format updated

--- Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/7.diff-var-let-const/hoisting.js
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/7.diff-var-let-const/hoisting.js
@@ -1,9 +1,9 @@
 //জাভাস্ক্রিপ্ট এর var, let, const এর মধ্যে প্রার্থক্য কি? 
 /* 
 Keyword	    Can be reassigned	    Can be redeclared	    scope	            hoisting
------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
 var	        Yes	                    Yes	                    Function Scope	    Yes
-let	        Yes	                    No	                    Block Scope	        No
+let	        Yes	                    No	                    Block Scope	            No
 const	        No	                    No	                    Block Scope	        No
 */
 
