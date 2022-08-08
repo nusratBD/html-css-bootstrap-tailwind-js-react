97aa81ecac87c4230f8312dab29dc50e6abf6ac5
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 10:29:16 +0600

    filter

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/9.filter.js
@@ -0,0 +1,5 @@
+//৬. অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+let result = numbers.filter(num=>num%2!=0);
+console.log(result);
