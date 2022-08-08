cd2c0be5b02ed0cd4b9f5d48c35c35e89d1c23f5
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 10:17:04 +0600

    var vs let/const

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/7.diff-var-let-const/hoisting.js
@@ -0,0 +1,25 @@
+//জাভাস্ক্রিপ্ট এর var, let, const এর মধ্যে প্রার্থক্য কি? 
+/* 
+Keyword	    Can be reassigned	    Can be redeclared	    scope	            hoisting
+-----------------------------------------------------------------------------------------
+var	        Yes	                    Yes	                    Function Scope	    Yes
+let	        Yes	                    No	                    Block Scope	        No
+const	    No	                    No	                    Block Scope	        No
+*/
+
+//Hoisting: Hoisting is a JS default behaviour where variables and function declarations are moved to the top of their scope before code execution
+
+//01: function: hoisting: true
+x();//output: Hello(initializes with proper value)
+function x(){
+    console.log('Hello');
+}
+
+//02. var: hoisting = true;(initializes with undefined)
+console.log(product);//(initializes with undefined)
+var product = 'car';//output: undefined. As the variable initialized after accessing it, it will initialize with undefined at the accessing place
+
+//03. let const
+console.log(item);//output: ReferenceError: Cannot access 'item' before initialization
+let item = 'car';
+
---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/7.diff-var-let-const/scope.js
@@ -0,0 +1,26 @@
+//জাভাস্ক্রিপ্ট এর var, let, const এর মধ্যে প্রার্থক্য কি? 
+/* 
+Keyword	    Can be reassigned	    Can be redeclared	    scope	            hoisting
+-----------------------------------------------------------------------------------------
+var	        Yes	                    Yes	                    Function Scope	    Yes
+let	        Yes	                    No	                    Block Scope	        No
+const	    No	                    No	                    Block Scope	        No
+*/
+//Block Scope and Function Scope
+//01. let const: block scope: scope works only inside of a block
+function x(){
+    if(true){
+        let name = 'Nusrat Hurain';
+    }
+    console.log(name);
+}
+x();//output: error. because let and const are block scoped. That means if it is used in a block, we can not use it outside of block although the block exixts in a function
+
+//02. var: function scope: scope works inside both the block and function
+function y(){
+    if(true){
+        var name = 'Nusrat Hurain';
+    }
+    console.log(name);
+}
+y();//output: Nusrat Hurain. As var is a function scoped variable, it accessed in the function although it is used inside of a block
