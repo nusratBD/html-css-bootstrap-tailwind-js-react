00dd50ebfff881302587ecdd8f898afca5573a7c
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 10:47:08 +0600

    Summary of map, forEach, filter, and find

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/11.summary:map-forEach-filter-find.js
@@ -0,0 +1,24 @@
+//জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+
+//01. map
+/* 
+map takes an array, and then do necessary calculation on the value of the array. After that it pushes the new value to a new array and return the new array
+*/
+
+//02. forEach
+/* 
+same as map with a littele difference. 
+It takes the array and calculate on the value of the array. Then create a new array push the calculate value inside the new array. But it doesn't return anything.
+*/
+
+//03: filter
+/* 
+It takes the array and implements a condition on the value of the array. The values those meet the condtion will remain and others will be removed by it. Then it creates a new array and push the granted value through filter in it. Finally it returns the new array.
+*/
+
+//04: find
+/* 
+Same as filter with a little difference.
+It takes an array and implements a condtion on the values of the array. Then according to the condition it takes only the first value that meets the condition and push in a new array and return it.
+
+*/
