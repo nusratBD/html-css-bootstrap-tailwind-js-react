de4d7fa3bd48155889640d78e753600630311c88
nusratBD <nusrat.hurain15@gmail.com>
2022-08-08 10:54:44 +0600

    array destructuring.js

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module32.5:ES6-Recap-Practice/13.array-destructuring.js
@@ -0,0 +1,4 @@
+//৯. array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে। 
+const mySelf = ['Nusrat Hurain', 25, 'Software Engineer', 50000000];
+[one, two, three, four] = mySelf;
+console.log(three);
