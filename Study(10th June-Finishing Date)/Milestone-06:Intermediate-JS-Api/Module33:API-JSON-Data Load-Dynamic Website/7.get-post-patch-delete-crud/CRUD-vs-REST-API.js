2925812aea088e0b9359cd0b3ecec64c33583eec
nusratBD <nusrat.hurain15@gmail.com>
2022-08-09 12:28:08 +0600

    CRUD vs REST API

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module33:API-JSON-Data Load-Dynamic Website/7.get-post-patch-delete-crud/CRUD-vs-REST-API.js
@@ -0,0 +1,5 @@
+/* 
+1.REST is an architectural system centered around resources and hypermedia, via HTTP protocols
+2.CRUD is a cycle meant for maintaining permanent records in a database setting
+3.CRUD principles are mapped to REST commands to comply with the goals of RESTful architecture
+*/
