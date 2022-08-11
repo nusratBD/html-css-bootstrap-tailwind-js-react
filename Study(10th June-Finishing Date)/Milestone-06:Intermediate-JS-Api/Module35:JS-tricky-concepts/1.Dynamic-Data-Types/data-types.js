82106795b6a0eab2269fc17094ac893d4541a118
nusratBD <nusrat.hurain15@gmail.com>
2022-08-11 20:39:56 +0600

    Dynamic Data Types

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module35:JS-tricky-concepts/1.Dynamic-Data-Types/data-types.js
@@ -0,0 +1,6 @@
+/* Dynamic data types are dynamic in nature and don't require initialization at the time of declaration. It also means that a dynamic type does not have a predefined type and can be used to store any type of data. JS is a dynamicaly typed variable. Because its type defines on its rigth side's value. */
+
+// const a = 25;
+// const a = 'Nusrat Hurain';
+const a = true;
+console.log(typeof a);
