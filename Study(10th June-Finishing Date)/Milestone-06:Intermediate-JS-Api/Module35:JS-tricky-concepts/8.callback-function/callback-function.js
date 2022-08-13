195f1e480b92b52831af22ddcd368687c5dd8721
nusratBD <nusrat.hurain15@gmail.com>
2022-08-13 18:43:01 +0600

    callback function

--- Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module35:JS-tricky-concepts/8.callback-function/callback-function.js
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module35:JS-tricky-concepts/8.callback-function/callback-function.js
@@ -1,7 +1,25 @@
 /* 
-At the time of calling a function if we pass a function as a parameter of the calling function, this calling function is called callback function.
+At the time of calling a function if we pass an annonymous function as a parameter of the calling function, this annonymous function is called callback function.
 
 Example: document.getElementById('my-btn).addEventListener('click', function(){
 
 })
 */
+function test(name,print){
+    print(name);
+}
+test('Nusrat',function(name){
+    console.log('Hi', name);
+})
+
+//Multiple Callback Function
+function test2(data, callback1, callback2){
+    console.log(data);
+    callback1(data);
+    callback2(data);
+}
+test2('Nusrat Hurain: Software Engineer', function(data){
+    console.log(data.toUpperCase())
+}, function(data){
+    console.log(data.toLowerCase())
+})
