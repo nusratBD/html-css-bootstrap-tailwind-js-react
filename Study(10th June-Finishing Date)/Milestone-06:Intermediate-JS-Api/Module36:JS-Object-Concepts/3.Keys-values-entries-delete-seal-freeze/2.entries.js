702e0317600d456f23a9692249d5b62511f7706e
nusratBD <nusrat.hurain15@gmail.com>
2022-08-14 10:33:25 +0600

    Object entries and delete

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module36:JS-Object-Concepts/3.Keys-values-entries-delete-seal-freeze/2.entries.js
@@ -0,0 +1,8 @@
+const self = {
+    name: 'Nusrat Hurain',
+    age: 25,
+    profession: 'Software Engineer',
+    salary: 50000000
+}
+const entries = Object.entries(self);
+console.log(entries);
---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module36:JS-Object-Concepts/3.Keys-values-entries-delete-seal-freeze/3.delete.js
@@ -0,0 +1,8 @@
+const self = {
+    name: 'Nusrat Hurain',
+    age: 25,
+    profession: 'Software Engineer',
+    salary: 50000000
+}
+delete self.age;
+console.log(self);;
