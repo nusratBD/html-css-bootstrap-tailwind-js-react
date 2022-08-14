607332519a7f7c46c7b73faf626606e8adc4cdb6
nusratBD <nusrat.hurain15@gmail.com>
2022-08-14 10:13:49 +0600

    Object Property

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module36:JS-Object-Concepts/2.Object-Method-Property/object-property.js
@@ -0,0 +1,26 @@
+const student = {
+    id:101,
+    name:'Nusrat Hurain',
+    major:'Software Engineering',
+    subjects:['math','physics','chemistry','english', 'programming fundamentals', 'economics'],
+    hobbies:{
+        indoor: ['book reading, cooking, gardening'],
+        outdoor: 'travelling'
+    },
+    isHardWorker: true,
+    job: function(){
+        console.log(this.name, 'will get a job very soon, ingSaAllah.');
+    },
+    income: 50000000,
+    donation: function(don, monthlyCost){
+        this.income = this.income - don - monthlyCost;
+        return this.income;
+    }
+}
+
+//Accessing Object Property
+student.job();
+const currentAmount = student.donation(10000000, 10000);
+console.log(currentAmount);
+const currentAmount2 = student.donation(5000000, 10000);
+console.log(currentAmount2);
