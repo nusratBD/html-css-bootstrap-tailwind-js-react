69a46da08316b8de2bd5d18c2a55d256bf91ea05
nusratBD <nusrat.hurain15@gmail.com>
2022-08-14 10:17:34 +0600

    Object Keys and Values

---
+++ Study(10th June-Finishing Date)/Milestone-06:Intermediate-JS-Api/Module36:JS-Object-Concepts/3.Keys-values-entries-delete-seal-freeze/1.keys-values.js
@@ -0,0 +1,10 @@
+const self = {
+    name: 'Nusrat Hurain',
+    age: 25,
+    profession: 'Software Engineer',
+    salary: 50000000
+}
+const keys = Object.keys(self);
+console.log(keys);
+const values = Object.values(self);
+console.log(values);
