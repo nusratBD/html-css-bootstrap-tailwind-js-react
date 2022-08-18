8713069112a6c9bd6037eb3ec315dfd75bc224d0
nusratBD <nusrat.hurain15@gmail.com>
2022-08-18 21:22:12 +0600

    How internet works

--- Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module38:How-JS-Browser-Works/1.How-internet-works.js
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module38:How-JS-Browser-Works/1.How-internet-works.js
@@ -1,4 +1,18 @@
 /* 
-Every electronics device has a unique IP address.
+Every electronic device has a unique address known as IP(Internet Protocol) address.
 Every website has a unique IP address that is known as DNS.
+*/
+
+/* 
+How does the internet work?
+Ans: When we search anything in our search bar, it first goes to the DNS server via the router and ISP. It then researchs the domain(name) that we searched is really exist or not. If it exists, it finds out the IP address for that particular domain. Then according to the IP address ISP manages(its own server or from central server) the desire data and sends to our local device.
+*/
+
+/* 
+www: The World Wide Web. It is the place where all the data of internet are stored.
+http: Hypertext Transfer Protocol, https: Hypertext Transfer Protocol Secure. This two are the road to go PC to www and backs www to PC. All our data pass PC to www and www to PC either in http or https. http data is readable, and https data is non-readable(decrypted). As a result http data can be hack, and https data can not be hacked.
+*/
+
+/* 
+tcp: Transmission Control Protocol. TCP organizes data so that it can be transmitted between a server and a client. It guarantees the integrity of the data being communicated over a network. Before it transmits data, TCP establishes a connection between a source and its destination, which it ensures remains live until communication begins. It then breaks large amounts of data into smaller packets, while ensuring data integrity is in place throughout the process. As a result, data reaches very fast from server to client.
 */
