39a2de3c3ba6bb8bb4b38786042779f0d3674c4d
nusratBD <nusrat.hurain15@gmail.com>
2022-08-19 00:15:25 +0600

    How does the browser work

---
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module38:How-JS-Browser-Works/2.How-browser-works/1.how-browser-works.js
@@ -0,0 +1,8 @@
+/* 
+Browser is a kind of desktop software that is used by installing in PC. The steps of showing a static website on a browser are:
+1. Browser converts our html file into a tree which is known as dom tree.
+2. To provide style on the html file, it creates a style tree.
+3. DOM tree and style tree together creates the render tree which indicates the layout of the website. If any update includes in the DOM tree or the render tree, it re-renders on the same process. 
+4. After getting render tree, it paints on it. If re-render happens, it does re-paint.
+5. At last the output is displayed on the UI(User Interface).
+*/
