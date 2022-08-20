b3353752f10de3fb07883177fe9cc38024722b22
nusratBD <nusrat.hurain15@gmail.com>
2022-08-20 12:57:20 +0600

    DOM BOM Browser API

---
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/1.BOM-DOM-BrowserAPI/1.DOM-browserAPI.js
@@ -0,0 +1,16 @@
+/* 
+DOM: Document Object Model. It works with browser not with the JS.
+Example: document.getElementById('test');
+
+It is called DOM(Document Object Model), because it is similar to object property access. 
+Example: 
+const self = {
+    name: 'Nusrat',
+    age: 25
+}
+access: self.name;
+*/
+
+/* 
+As DOM is only works in browser, it is called browser API.
+*/
---
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/1.BOM-DOM-BrowserAPI/2.BOM.js
@@ -0,0 +1,15 @@
+/* 
+BOM: Browser Object Model. DOM is the part of BOM.
+
+3
+
+BOM means Browser Object Model . These are objects that you can use to manipulate the browser. they are navigator
+1.navigator
+2.screen
+3.location
+4.history
+5.document
+they are all children of the Window Object. DOM is Document Object Model is part of the BOM and it helps you manipulate the content of the loaded page file. this include the HTML and CSS
+
+#DOM only works with document. BOM works with whole window.
+*/
