99853804ed36f909b696c3896efdaf4b411c1484
Nusrat Hurain <56173472+nusratBD@users.noreply.github.com>
2022-08-23 00:44:58 +0600

    Update browserapi-webapi.js
    webapi is the subset of browserapi.

---
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/9.Content-Editable/browserapi-webapi.js
@@ -0,0 +1,5 @@
+/*
+The term Web APIs is a very generic term used in a lot of different instances. For this blog post I want to talk about Web APIs in the context of the browser. Basically, Web APIs are the APIs made available to us, front-end developers, by the browser. They are also referred to as BOM (Browser Object Model) APIs. For example, the DOM API is a BOM API, more specifically, it is a subset of the BOM APIs. Another example would be the Event interface or the Element interface, which both are part of the DOM API and consequently are also part of the BOM APIs.
+
+So, a Web API, in the context of the browser, simply is an API, provided by the browser and that we can communicate with using JavaScript in order to solve our front-end problems. Even though these APIs are accessible with JavaScript, their implementation is in the language that the browser uses, for example, for Google Chrome it is C++.
+*/
