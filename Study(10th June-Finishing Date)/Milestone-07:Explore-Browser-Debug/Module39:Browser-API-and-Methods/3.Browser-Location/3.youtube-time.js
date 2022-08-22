a9b137f110917d6d15fa5b72d7de74aedab47fd1
Nusrat Hurain <56173472+nusratBD@users.noreply.github.com>
2022-08-23 00:35:28 +0600

    Update 3.youtube-time.js

--- Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/3.Browser-Location/3.youtube-time.js
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/3.Browser-Location/3.youtube-time.js
@@ -1,3 +1,3 @@
 /* 
-When we play a youtube video, on the search bar it add &t=50(any seconds), it will play from that particular second.
+When we play a youtube video, on the search bar if we add &t=50(any seconds), it will play from that particular second.
 */
