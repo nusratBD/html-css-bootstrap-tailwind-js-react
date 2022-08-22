6ba775ffc722f8c0d20ab353d21be182e8a66b84
nusratBD <nusrat.hurain15@gmail.com>
2022-08-22 12:42:11 +0600

    temperature api

---
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/9.Content-Editable/chrome-short-cut.js
@@ -0,0 +1,7 @@
+/* 
+1) open new tab on existing tab: chrome://newtab
+2) back to the previous site: Ctrl + Shift + t
+3) Who are using most of the memory space: Shift + Esc
+4. chrome://restart
+5. Select URL: Ctrl + L
+*/
