76a37b320da712ddf9f3833314786c0876525e46
Nusrat Hurain <56173472+nusratBD@users.noreply.github.com>
2022-08-26 06:07:41 +0600

    cookie defination

--- Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/5.cookie/1.defination.js
+++ Study(10th June-Finishing Date)/Milestone-07:Explore-Browser-Debug/Module39:Browser-API-and-Methods/5.cookie/1.defination.js
@@ -7,5 +7,5 @@ To put it simply, cookies are a bit like getting a ticket for a coat check:
 
 You hand over your “coat” to the cloak desk. In this case, a pocket of data is linked to you on the website server when you connect. This data can be your personal account, your shopping cart, or even just what pages you’ve visited.
 You get a “ticket” to identify you as the “coat” owner. The cookie for the website is given to you and stored in your web browser. It has a unique ID especially for you.
-If you leave and return, you can get the “coat” with your “ticket”. Your browser gives the website your cookie. It reads the unique ID in the cookie to assemble your activity data and recall your visit just as you left it
+If you leave and return, you can get the “coat” with your “ticket”. Your browser gives the website your cookie. It reads the unique ID in the cookie to assemble your activity data and recall your visit just as you left it.
 */
